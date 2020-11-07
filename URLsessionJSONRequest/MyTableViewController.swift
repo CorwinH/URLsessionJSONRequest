c0766a84de353bb0b41048e81df54558f56f2616
Corwin Hil - Mac <Corwin@L3git.com>
2020-11-07 14:16:02 -0500

    Moved the Data into the TableView, had issues making it work on the main... it works now. Loads data and reloads the tableview ones data is loaded.

--- URLsessionJSONRequest/MyTableViewController.swift
+++ URLsessionJSONRequest/MyTableViewController.swift
@@ -10,31 +10,143 @@ import UIKit
 class MyTableViewController: UITableViewController {
     
     // Demo Array how to populate the Table View contoller
-    public var mobles: [String] = [
-        "Ones",
-        "Two",
-        "Three",
-        "four",
-        "five",
-        "Six"
-    ]
+    public var mobles: [String] = []
+    private var loading = true
+    
+    
+    
+    
+    
+   
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell") 
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        theapi()
+        
+        
     }
     
     // How many Table Rows ??
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return mobles.count // Count the array so we know how many rows to show.
+        func test(n:NSNotification){
+            
+        }
+        if loading{
+            return 1
+        } else{
+            return mobles.count // Count the array so we know how many rows to show.
+        }
+        
+        
+        
+       
+        
     }
     
     // Build the Cell
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) // identifer
-        cell.textLabel?.text = mobles[indexPath.row] // The Text for the cell
+        
+        if loading {
+            cell.textLabel?.text = "...."
+        } else{
+            cell.textLabel?.text = mobles[indexPath.row] // The Text for the cell
+        }
         return cell // call the cell
+    }
+    
+    private func theapi(){
+        /**/
+        
+        // Calls the URL (API)
+        guard  let url = URL(string: "https://l3dev.com/api/workers") else{return}
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: url) { [self] (data, response, error) in
+            guard let data = data else { return }
+                do{
+                    /*let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)*/
+                    
+                    let users = try JSONDecoder().decode(UserResponse.self, from: data)
+                    
+                    //print(users)
+                    mobles.removeAll()
+                    var i = 0
+                    
+                    
+                    // Parsing the Data within a For Each loop
+                    users.users.forEach {
+                        print($0.first_name)
+                        mobles.append($0.first_name) // append to numbers not working..
+                        //print(users.users.endIndex)
+                        i = i+1
+                        //print(i)
+                        /*
+                        if(users.users.endIndex == i){
+                            //fox = 2
+                            //print(fox)
+                            // push here.. but how???
+                            NotificationCenter.default.post(name: NSNotification.Name("CountMadeIt"), object: nil)
+                            
+                            print("xxx")
+                            // adding our array to our Table View Controller
+                            //vc.mobles = numbers
+                            // Load the Table View Contoller page on load.
+                            //self.navigationController?.pushViewController(vc, animated: true)
+                            
+                            
+                        }*/
+                        
+                    }
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    /*
+                    for user in users{
+                        print([user.users])
+                    }
+    */
+                    
+                    
+                    //print("ID -> " + users.first_name)
+                    
+                    /* if array using decode([User].self,....)*/
+    /*(
+                    for user in users{
+                        print(user.id)
+                    }
+    */
+                    
+                } catch{ print(error) } // Print error later push to logs
+            
+            self.loading = false
+            // cant run in main?? Then use DispatchQueue.main.asyc then it will F! work.
+            DispatchQueue.main.async {
+                tableView.reloadData()
+            }
+            
+            
+            }
+        
+        
+        //NotificationCenter.default.addObserver(self, selector: #selector(didcount(n:)), name: NSNotification.Name.init(rawValue: "DataDownloaded"), object: nil)
+        
+        
+        task.resume() // reuse normal use
+        
+        
+        /***/
     }
    
 
--- URLsessionJSONRequest/ViewController.swift
+++ URLsessionJSONRequest/ViewController.swift
@@ -13,16 +13,22 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    
+    
     //var users = [UserResponse]()
     
     // The String we'll use to push everything to our Table view
-    var numbers: [String] = ["ABC123", "i'm the best", "not really", "some more random text"]
+    var numbers: [String] = ["ABC123"]
+    var fox = 0
+    
+    
     
 
     @IBAction func onGetTapped(_ sender: Any) {
         
         // Declaring the table View Controller for when we push our data to the next page.
         let vc = MyTableViewController()
+        //var psuhit = 0
         
         // Calls the URL (API)
         guard  let url = URL(string: "https://l3dev.com/api/workers") else{return}
@@ -35,17 +41,38 @@ class ViewController: UIViewController {
                     print(json)*/
                     
                     let users = try JSONDecoder().decode(UserResponse.self, from: data)
-                    //print(users)
                     
+                    //print(users)
+                    numbers.removeAll()
+                    var i = 0
                     
                     
                     // Parsing the Data within a For Each loop
                     users.users.forEach {
                         print($0.first_name)
-                        self.numbers.append("33xxxxxxxxxxxxxxxxxx") // append to numbers not working..
-                        numbers += ["3321"] // test
-                        self.numbers += ["3321"] // test
+                        self.numbers.append($0.first_name) // append to numbers not working..
+                        //print(users.users.endIndex)
+                        i = i+1
+                        //print(i)
+                        if(users.users.endIndex == i){
+                            fox = 2
+                            print(fox)
+                            // push here.. but how???
+                            //NotificationCenter.default.post(name: NSNotification.Name("CountMadeIt"), object: nil)
+                            
+                            print("xxx")
+                            // adding our array to our Table View Controller
+                            vc.mobles = numbers
+                            // Load the Table View Contoller page on load.
+                            self.navigationController?.pushViewController(vc, animated: true)
+                            
+                            
+                        }
+                        
                     }
+                    
+                    
+                    
                     
                     
                     
@@ -67,16 +94,43 @@ class ViewController: UIViewController {
     */
                     
                 } catch{ print(error) } // Print error later push to logs
+            
+           
+            
             }
+        
+        
+        //NotificationCenter.default.addObserver(self, selector: #selector(didcount(n:)), name: NSNotification.Name.init(rawValue: "DataDownloaded"), object: nil)
+        
+        
+        
+           
+        
+        
+            
+        
+        
+        
+        
         task.resume() // reuse normal use
         
-        // adding our array to our Table View Controller
-        vc.mobles = numbers
-        // Load the Table View Contoller page on load.
-        navigationController?.pushViewController(vc, animated: true)
+        
+        
+        
+        
+        func didcount(n:NSNotification){
+                
+                
+                
+            }
+        
+        
+        
         
         
     }
+    
+    
     
     
     
@@ -111,6 +165,8 @@ class ViewController: UIViewController {
         
         
     }
+    
+    
     
 
 }
